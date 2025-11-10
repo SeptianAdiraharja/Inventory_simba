7ebd020143b50bd564024ef4f3382abe983b6eec
hanahanoo <sofiatunnisahana@gmail.com>
2025-11-10 13:25:47 +0700

    update revisi pegawai

--- app/Http/Controllers/PermintaanController.php
+++ app/Http/Controllers/PermintaanController.php
@@ -17,9 +17,9 @@ class PermintaanController extends Controller
         $categories = Category::all();
         $items = Item::with('category')
             ->withSum('cartItems as total_dibeli', 'quantity')
-            ->orderByRaw('CASE WHEN stock > 0 THEN 0 ELSE 1 END') // logic stock 0/ habis
             ->orderByDesc('total_dibeli')
             ->orderByDesc('created_at') // misal kalau sama ambil yang baru dibuat
+            ->where('stock', '>', 0)
             ->paginate(12);
 
         return view('role.pegawai.produk', compact('categories', 'items'));
